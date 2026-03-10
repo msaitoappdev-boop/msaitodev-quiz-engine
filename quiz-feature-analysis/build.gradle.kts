a44247ffd26f3382f70e1e01d07af96611f44f88
msait <msaitoappdev@gmail.com>
2026-03-11 00:13:01 +0900

    正解率詳細をプライマリーカラーで表示するように修正

--- quiz-feature-analysis/build.gradle.kts
+++ quiz-feature-analysis/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.msaitodev.quiz"
                 artifactId = "quiz-feature-analysis"
-                version = "1.0.0"
+                version = "1.0.1"
                 from(components["release"])
             }
         }
